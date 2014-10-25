c86737dca0b8a07b314aa0127be8365000fac33e
mateusz <mateuszalmannai@googlemail.com>
2014-10-25 17:24:38 +0200

    add first hierarchy

---
+++ have_a_purpose/db_backup/bin/db_backup_initial.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+databases = {
+  big_client :{
+    database :'big_client',
+    username :'big',
+    password :'big',
+          },
+small_client :{
+    database :'small_client',
+    username :'small'
+    password :'p@ssWord!',
+}
+}
